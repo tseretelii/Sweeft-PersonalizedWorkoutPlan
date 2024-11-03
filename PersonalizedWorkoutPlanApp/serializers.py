5dea6c17fe7408b8aa84f7242f9d8864e7b3f456
tseretelii <tsereteligigi01@gmail.com>
2024-11-03 18:37:54 +0400

    Optimized User Creation

--- PersonalizedWorkoutPlanApp/serializers.py
+++ PersonalizedWorkoutPlanApp/serializers.py
@@ -5,6 +5,8 @@ class AppUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = AppUser
         fields = [
+            "id",
+            "username",
             "first_name",
             "last_name",
             "email",
@@ -14,6 +16,26 @@ class AppUserSerializer(serializers.ModelSerializer):
             "age",
             "gender",
         ]
+        extra_kwargs = {            # By Adding This code,
+            'password': {           # password becomes write only,
+                'write_only': True  # it does not appear on get request
+            }
+        }
+
+    def create(self, validated_data):
+        user = AppUser(
+            username=validated_data['username'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            email = validated_data['email'],
+            weight = validated_data['weight'],
+            height = validated_data['height'],
+            age = validated_data['age'],
+            gender = validated_data['gender'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class MuscleSerializer(serializers.ModelSerializer):
     class Meta:
